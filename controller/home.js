5d2769674528a6db03a9c1d0fc335cf8450d7890
shinderaju <shinderaju08@gmail.com>
2016-09-10 17:40:22 +0530

    ipl final commented code

--- controller/home.js
+++ controller/home.js
@@ -2,27 +2,24 @@
  * FileName:home.js
  * CreatedBy: raju
  * Date :30-08-2016
- *
+ * Purpose :controller for geting info all the teams
  */
-
-/**
- * controller for geting into all the teams
- */
-angular.module('myApp').controller('team_ctrl', demo);
-
-/*
- * Purpose : function to set object of firebase
- */
+angular.module('myApp').controller('homeCtrl', demo);
 function demo($scope, $firebaseObject, teamService) {
     //get firebase object of teaminfo
     var fbref = firebase.database().ref("tean_info");
     var fbObject = $firebaseObject(fbref);
+    // fetch general data of all the teams
     fbObject.$loaded().then(function(obj) {
-        $scope.data = obj;
+        $scope.teamsData = obj;
     });
+
     $scope.changeTeam = function(teamName) {
         teamService.setTeam(teamName);
     };
     $scope.teamName = teamService.getTeam();
 
+
 };
+{/* <div ng-controller="imgCtrl" ng-init="{{getUrl($parent.teamData.team_img_url)}}">
+  <img src="{{imgSrc}}"></img> */}
